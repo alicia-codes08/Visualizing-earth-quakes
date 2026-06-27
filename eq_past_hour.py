d5846fdd9438a5db043d8c2b39234fbee0eff49a
Alicia <Alicia@sigma.com>
2026-06-28 01:27:25 +0200

    Finished second visual

--- eq_past_hour.py
+++ eq_past_hour.py
@@ -2,3 +2,31 @@ from pathlib import Path
 import json 
 
 import plotly.express as px
+
+path = Path("eq_data/all_hour.geojson")
+contents = path.read_text(encoding="utf-8")
+all_data = json.loads(contents)
+
+# make readable geojson-file
+path = Path("eq_data/readalbe_all_hour.geojson")
+readable_contents = json.dumps(all_data, indent=4)
+path.write_text(readable_contents)
+
+# extract data
+all_eq = all_data["features"]
+mags, lons, lats, eq_names = [], [], [], []
+for eq in all_eq:
+    mag = eq["properties"]["mag"]
+    if mag > 0:
+        mags.append(mag)
+        lons.append(eq["geometry"]["coordinates"][0])
+        lats.append(eq["geometry"]["coordinates"][1])
+        eq_names.append(eq["properties"]["title"])
+
+# visualize data
+title = all_data["metadata"]["title"]
+fig = px.scatter_geo(lon=lons, lat=lats, size=mags, title=title, color=mags, 
+                    color_continuous_scale='viridis', 
+                    projection="natural earth", hover_name=eq_names, 
+                    labels={"color": "Magnitude"}) 
+fig.show() 
